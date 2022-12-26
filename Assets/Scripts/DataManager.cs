e39184d0be62e945f9fe66091e5b4bf2f2db5e2a
SKYlight Studio <jamespk0725@gmail.com>
2022-12-26 21:06:53 +0900

    first commit

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public int id;
+    public bool isPlayable;
+
+    // 0 : 유진
+    // 1 : 네로
+    // 2 : 네로 얼티밋
+    // 3 : 판오스의 카타리나
+}
